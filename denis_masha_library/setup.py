0e99878c616befaaebd4a6988296edcf220e313b
DenisProcyon <denis.shadrin@procyon.ee>
2024-10-18 13:07:21 +0200

    move functions to library

--- denis_masha_library/setup.py
+++ denis_masha_library/setup.py
@@ -6,13 +6,16 @@ setup(
     description="Functions for hw4",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author="Your Name",
+    author="Maria Simakova",
     author_email="maria.simakova@bse.eu",
     url="https://github.com/mariasimakova/denis_masha_hw4",  
     packages=find_packages(),
     install_requires=[
+        "pathlib",
         "pandas",
         "geopy",
+        "scikit-learn",
+        "matplotlib",
     ],
     python_requires='>=3.12',
 )
